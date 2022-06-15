a95945bcb525b8835f8d1ce5765b643bf698dd72
GrDereken <grdereken@gmail.com>
2022-06-15 22:00:10 +0300

    Added the rest of the code

--- Mobile/app/src/main/java/com/gelkalampakas/schoolauthenticator/models/LoginData.kt
+++
@@ -1,7 +0,0 @@
-package com.gelkalampakas.schoolauthenticator.models
-
-class LoginData(val username: String, val password: String) {
-    fun isValid(): Boolean {
-        return username != ""
-    }
-}
