25e284a7e1b794e72a1226504bbf628912e821ac
Thanasis Vlioras <athanasiosvlioras@gmail.com>
2022-06-18 22:49:00 +0300

    Added "You are the active user!" prompt to mobile

--- code/Mobile/app/src/main/java/com/gelkalampakas/schoolauthenticator/activities/MainActivity.kt
+++ code/Mobile/app/src/main/java/com/gelkalampakas/schoolauthenticator/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.widget.Button
 import android.widget.Switch
 import android.widget.TextView
@@ -20,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
 
     private lateinit var userPointsView: TextView
+    private lateinit var activeUserPromptView: TextView
+
     private lateinit var activeUserModeSwitch: Switch
 
     private lateinit var setActiveUserButton: Button
@@ -42,9 +45,10 @@ class MainActivity : AppCompatActivity() {
         toolbar.setOnMenuItemClickListener { toolbarHandler(this, it) }
 
         userPointsView = findViewById(R.id.userPointsView)
+        activeUserPromptView = findViewById(R.id.activeUserPromptView)
 
         activeUserModeSwitch = findViewById(R.id.activeUserModeSwitch);
-        activeUserModeSwitch.setOnClickListener {updateUserPoints()}
+        activeUserModeSwitch.setOnClickListener {updateUI()}
 
         setActiveUserButton = findViewById(R.id.setActiveUserButton)
         setActiveUserButton.setOnClickListener {setActiveUser()}
@@ -107,29 +111,39 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun updateUserPoints() {
+    private fun updateUserPointsView() {
         val activeUserMode = activeUserModeSwitch.isChecked
 
         val loggedInUser = preferenceHandler.getLoginData().username
 
         val newText = if (activeUserMode) String.format("Active User: %s\nPoints: %s", activeUser, activeUserPoints)
-            else String.format("Logged In User: %s\nPoints: %s", loggedInUser, loggedInUserPoints)
+        else String.format("Logged In User: %s\nPoints: %s", loggedInUser, loggedInUserPoints)
 
         userPointsView.setText(newText)
     }
 
+    private fun updateActiveUserPrompt() {
+        val loggedInUser = preferenceHandler.getLoginData().username
+        activeUserPromptView.visibility = if(activeUser == loggedInUser) View.VISIBLE else View.INVISIBLE
+    }
+
+    private fun updateUI() {
+        updateUserPointsView()
+        updateActiveUserPrompt()
+    }
+
     private fun getActiveUser(newActiveUser: String) {
         activeUser = newActiveUser
-        updateUserPoints()
+        updateUI()
     }
 
     private fun getActiveUserPoints(newPoints: String) {
         activeUserPoints = newPoints
-        updateUserPoints()
+        updateUI()
     }
 
     private fun getLoggedInUserPoints(newPoints: String) {
         loggedInUserPoints = newPoints
-        updateUserPoints()
+        updateUI()
     }
 }
